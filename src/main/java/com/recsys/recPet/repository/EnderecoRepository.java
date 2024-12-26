52dc61a0d5e67d721aaaf0b3ee527f37f8be311b
Milena <elenprojects7@gmail.com>
2024-12-26 14:42:03 -0300

    feat: adiciona repository

---
+++ src/main/java/com/recsys/recPet/repository/EnderecoRepository.java
@@ -0,0 +1,6 @@
+package com.recsys.recPet.repository;
+
+import com.recsys.recPet.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> { }
