761f66c459ae6c165da49c668a9bdd568dbf72cd
Milena <elenprojects7@gmail.com>
2025-02-02 15:33:07 -0300

    feat: troca blank for null

--- src/main/java/com/recsys/recPet/dto/admin/UpdateRoleDTO.java
+++ src/main/java/com/recsys/recPet/dto/admin/UpdateRoleDTO.java
@@ -1,15 +1,17 @@
 package com.recsys.recPet.dto.admin;
 
 import com.recsys.recPet.enums.TipoUsuario;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateRoleDTO {
-    @NotBlank(message = "Tipo é obrigatório")
+    @NotNull(message = "Tipo é obrigatório")
     public TipoUsuario tipo;
 }
