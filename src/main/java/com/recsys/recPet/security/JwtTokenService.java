d41601294c1bb67702ef9e838e857d9bb0684cd2
Milena <elenprojects7@gmail.com>
2025-02-22 15:11:14 -0300

    config: variaveis de ambiente

--- src/main/java/com/recsys/recPet/security/JwtTokenService.java
+++ src/main/java/com/recsys/recPet/security/JwtTokenService.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.recsys.recPet.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,34 +14,38 @@ import java.time.ZonedDateTime;
 @Service
 public class JwtTokenService {
 
-    private static final String SECRET_KEY = "4Z^XrroxR@dWxqf$mTTKwW$!@#qGr4P";//IRÁ VIRAR VARIAVEL DE AMBIENTE
-    private static final String ISSUER = "recPet-api"; // Emissor do token
+    @Value("${jwt.security.key}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.security.issuer}")
+    private String ISSUER;
+
+    @Value("${jwt.security.expiration.hours}")
+    private Integer EXPIRATION_TIME;
 
     public String generateToken(User user) {
-        System.out.println("Gerando token para o usuário: " + user.getEmail());
         try {
-            // Define o algoritmo HMAC SHA256 para criar a assinatura do token passando a chave secreta definida
             Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
             return JWT.create()
-                    .withIssuer(ISSUER) // Define o emissor do token
-                    .withIssuedAt(creationDate()) // Define a data de emissão do token
-                    .withExpiresAt(expirationDate()) // Define a data de expiração do token
-                    .withSubject(user.getUsername()) // Define o assunto do token (neste caso, o nome de usuário)
+                    .withIssuer(ISSUER)
+                    .withIssuedAt(creationDate())
+                    .withExpiresAt(expirationDate())
+                    .withSubject(user.getUsername())
                     .withClaim("ROLE", user.getTipoUsuario().name())
-                    .sign(algorithm); // Assina o token usando o algoritmo especificado
+                    .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new JWTCreationException("Erro ao gerar token.", exception);
         }
     }
+
     public String getSubjectFromToken(String token) {
         try {
-            // Define o algoritmo HMAC SHA256 para verificar a assinatura do token passando a chave secreta definida
             Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
             return JWT.require(algorithm)
-                    .withIssuer(ISSUER) // Define o emissor do token
+                    .withIssuer(ISSUER)
                     .build()
-                    .verify(token) // Verifica a validade do token
-                    .getSubject(); // Obtém o assunto (neste caso, o nome de usuário) do token
+                    .verify(token)
+                    .getSubject();
 
 
         } catch (JWTVerificationException exception){
@@ -53,6 +58,6 @@ public class JwtTokenService {
     }
 
     private Instant expirationDate() {
-        return ZonedDateTime.now(ZoneId.of("America/Recife")).plusHours(120).toInstant();
+        return ZonedDateTime.now(ZoneId.of("America/Recife")).plusHours(EXPIRATION_TIME).toInstant();
     }
 }
