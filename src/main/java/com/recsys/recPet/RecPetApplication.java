1335c874e4aed44d2138254e9ce346b784c5c738
Milena <elenprojects7@gmail.com>
2025-02-22 14:00:14 -0300

    feat: EnableJpaRepositories e EntityScan

--- src/main/java/com/recsys/recPet/RecPetApplication.java
+++ src/main/java/com/recsys/recPet/RecPetApplication.java
@@ -2,8 +2,12 @@ package com.recsys.recPet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.recsys.recPet.repository")
+@EntityScan(basePackages = "com.recsys.recPet.model")
 public class RecPetApplication {
 
 	public static void main(String[] args) {
