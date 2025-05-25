44d388e77d06fc07727708e069aeac3dc2aff997
Milena <elenprojects7@gmail.com>
2025-05-24 22:24:57 -0300

    feat: add test-container

--- src/test/java/com/recsys/recPet/RecPetApplicationTests.java
+++ src/test/java/com/recsys/recPet/RecPetApplicationTests.java
@@ -1,13 +1,35 @@
 package com.recsys.recPet;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class RecPetApplicationTests {
 
-	@Test
-	void contextLoads() {
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+			.withDatabaseName("integration-tests-db")
+			.withUsername("sa");
+
+	@BeforeAll
+	static void beforeAll() {
+		postgres.start();
 	}
 
+	@AfterAll
+	static void afterAll() {
+		postgres.stop();
+	}
+
+	@Test
+	void testDockerConnection() {
+		assertTrue(postgres.isRunning());
+	}
 }
