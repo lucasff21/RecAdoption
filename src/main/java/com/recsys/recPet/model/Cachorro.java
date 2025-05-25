511e69bb9d9eb3b61f80af8ae5c82dd8e2f07263
Milena <elenprojects7@gmail.com>
2025-05-24 22:58:06 -0300

    feat(cachorro): inclui equals e hashcode para incluir campos da superclasse

--- src/main/java/com/recsys/recPet/model/Cachorro.java
+++ src/main/java/com/recsys/recPet/model/Cachorro.java
@@ -3,8 +3,10 @@ package com.recsys.recPet.model;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
