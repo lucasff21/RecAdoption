04917b4481e0c50014494e62ae8f3ade6af2e9bf
Milena <elenprojects7@gmail.com>
2025-06-30 21:02:36 -0300

    feat(adocao-repository): findByUserId

--- src/main/java/com/recsys/recPet/repository/AdocaoRepository.java
+++ src/main/java/com/recsys/recPet/repository/AdocaoRepository.java
@@ -4,6 +4,9 @@ import com.recsys.recPet.model.Adocao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
+    List<Adocao> findByUserId(Long userId);
 }
