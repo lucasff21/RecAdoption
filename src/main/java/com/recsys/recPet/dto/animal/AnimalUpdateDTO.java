5a4c5c9201b0c634db26948f20c76932fbfd28b3
Milena <elenprojects7@gmail.com>
2025-06-30 21:17:15 -0300

    feat(animal-update-dto)

---
+++ src/main/java/com/recsys/recPet/dto/animal/AnimalUpdateDTO.java
@@ -0,0 +1,43 @@
+package com.recsys.recPet.dto.animal;
+
+import com.recsys.recPet.enums.pet.Pelagem;
+import com.recsys.recPet.enums.pet.Porte;
+import com.recsys.recPet.enums.pet.Sexo;
+import com.recsys.recPet.model.Animal;
+import lombok.AllArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+import jakarta.validation.constraints.*;
+        import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List; // Novo
+import java.util.Optional;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AnimalUpdateDTO {
+    @Size(max = 100, message = "Nome deve ter no máximo 100 caracteres")
+    private String nome;
+
+    private LocalDate dataNascimentoAproximada;
+
+    private Sexo sexo;
+
+    private Porte porte;
+
+    private Pelagem pelagem;
+
+    private List<String> caracteristicas;
+
+    private MultipartFile novaImagem;
+
+    public void updateEntity(Animal animal) {
+        Optional.ofNullable(this.nome).ifPresent(animal::setNome);
+        Optional.ofNullable(this.dataNascimentoAproximada).ifPresent(animal::setDataNascimentoAproximada);
+        Optional.ofNullable(this.sexo).ifPresent(sexo -> animal.setSexo(sexo.name()));
+        Optional.ofNullable(this.porte).ifPresent(porte -> animal.setPorte(porte.name()));
+        Optional.ofNullable(this.pelagem).ifPresent(pelagem -> animal.setPelagem(pelagem.name()));
+    }
+}
