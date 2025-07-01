20320f7c58720ccaf4943f98a07aa9fc8603fb59
Milena <elenprojects7@gmail.com>
2025-06-30 21:01:57 -0300

    feat(adocao-controller): rota da lista de adocoes e retira o dataAdocao

--- src/main/java/com/recsys/recPet/controller/AdocaoController.java
+++ src/main/java/com/recsys/recPet/controller/AdocaoController.java
@@ -1,20 +1,20 @@
 package com.recsys.recPet.controller;
 
 import com.recsys.recPet.dto.AdocaoDTO;
+import com.recsys.recPet.dto.AdocaoResponseDTO;
 import com.recsys.recPet.enums.adocao.AdocaoStatus;
 import com.recsys.recPet.model.Adocao;
 
 import com.recsys.recPet.model.Animal;
 import com.recsys.recPet.model.User;
 import com.recsys.recPet.service.AdocaoService;
-import com.recsys.recPet.service.CachorroService;
+import com.recsys.recPet.service.AnimalService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -22,10 +22,9 @@ import java.util.List;
 public class AdocaoController {
 
     private final AdocaoService adocaoService;
-  
-    private final CachorroService cachorroService;
+    private final AnimalService cachorroService;
 
-    public AdocaoController(AdocaoService adocaoService, CachorroService cachorroService) {
+    public AdocaoController(AdocaoService adocaoService, AnimalService cachorroService) {
         this.adocaoService = adocaoService;
         this.cachorroService = cachorroService;
     }
@@ -34,7 +33,6 @@ public class AdocaoController {
     public ResponseEntity<Adocao> save(@RequestBody AdocaoDTO adocaoDTO, @AuthenticationPrincipal User user) {
         Adocao adocao = new Adocao();
         adocao.setUser(user);
-        adocao.setDataAdocao(LocalDate.now());
         adocao.setStatus(AdocaoStatus.PENDENTE);
         Animal animal = cachorroService.findById(adocaoDTO.animalId());
         if (animal == null) {
@@ -47,7 +45,7 @@ public class AdocaoController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Adocao> update(@PathVariable Long id, @RequestBody AdocaoDTO adocaoDTO){
+    public ResponseEntity<Adocao> update(@PathVariable Long id, @RequestBody AdocaoDTO adocaoDTO) {
         Adocao adocao = new Adocao();
         BeanUtils.copyProperties(adocaoDTO, adocao);
         adocao.setId(id);
@@ -57,20 +55,30 @@ public class AdocaoController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Adocao>> adocaoList(){
+    public ResponseEntity<List<Adocao>> adocaoList() {
         List<Adocao> adocaoList = adocaoService.findAall();
         return ResponseEntity.status(HttpStatus.OK).body(adocaoList);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Adocao> findById(@PathVariable Long id){
-        Adocao adoca = adocaoService.findById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(adoca);
+    public ResponseEntity<Adocao> findById(@PathVariable Long id) {
+        Adocao adocao = adocaoService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(adocao);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         adocaoService.delete(id);
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/usuario/{usuarioId}")
+    public ResponseEntity<List<AdocaoResponseDTO>> getAdocoesByUserId(@PathVariable Long usuarioId) {
+        try {
+            List<AdocaoResponseDTO> adocoes = adocaoService.getAdocoesByUserId(usuarioId);
+            return ResponseEntity.ok(adocoes);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
