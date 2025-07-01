d5501dfe4009011b06207b8fb974e34d5a2d1fc0
Milena <elenprojects7@gmail.com>
2025-06-30 21:14:08 -0300

    feat(animal-caracteristica-repository)

---
+++ src/main/java/com/recsys/recPet/repository/AnimalCaracteristicaRepository.java
@@ -0,0 +1,10 @@
+package com.recsys.recPet.repository;
+
+import com.recsys.recPet.model.AnimalCaracteristica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalCaracteristicaRepository extends JpaRepository<AnimalCaracteristica, Integer>
+{
+}
