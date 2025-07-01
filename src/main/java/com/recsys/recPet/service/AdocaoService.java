678e7f03a655e50040040407f584f306a1f472ca
Milena <elenprojects7@gmail.com>
2025-06-30 21:08:48 -0300

    feat(adocao-service): findAllAdocoesWithQuestionario e getAdocoesByUserId

--- src/main/java/com/recsys/recPet/service/AdocaoService.java
+++ src/main/java/com/recsys/recPet/service/AdocaoService.java
@@ -1,19 +1,32 @@
 package com.recsys.recPet.service;
 
+import com.recsys.recPet.dto.AdocaoResponseDTO;
+import com.recsys.recPet.dto.admin.AdminAdocaoDTO;
 import com.recsys.recPet.model.Adocao;
+import com.recsys.recPet.model.Questionario;
 import com.recsys.recPet.repository.AdocaoRepository;
+import com.recsys.recPet.repository.QuestionarioRepository;
+import com.recsys.recPet.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class AdocaoService {
     private final AdocaoRepository adocaoRepository;
+    private final QuestionarioRepository questionarioRepository;
+    private final UserRepository userRepository;
 
-    public AdocaoService(AdocaoRepository adocaoRepository) {
+    public AdocaoService(AdocaoRepository adocaoRepository, QuestionarioRepository questionarioRepository ,
+                         UserRepository userRepository) {
         this.adocaoRepository = adocaoRepository;
+        this.questionarioRepository = questionarioRepository;
+        this.userRepository = userRepository;
     }
 
     public Adocao save(Adocao adocao){
@@ -43,4 +56,24 @@ public class AdocaoService {
         adocaoRepository.delete(adocao);
     }
 
+    public Page<AdminAdocaoDTO> findAllAdocoesWithQuestionario(Pageable pageable) {
+        Page<Adocao> adocaoPage = adocaoRepository.findAll(pageable);
+
+        return adocaoPage.map(adocao -> {
+            Optional<Questionario> questionnaire = Optional.ofNullable(questionarioRepository.findByUser_Id(adocao.getUser().getId()));
+            return new AdminAdocaoDTO(adocao, questionnaire.orElse(null));
+        });
+    }
+
+    public List<AdocaoResponseDTO> getAdocoesByUserId(Long userId) {
+        if (!userRepository.existsById(userId)) {
+            throw new EntityNotFoundException("Usuário não encontrado");
+        }
+
+        List<Adocao> adocoes = adocaoRepository.findByUserId(userId);
+
+        return adocoes.stream()
+                .map(AdocaoResponseDTO::new)
+                .collect(Collectors.toList());
+    }
 }
