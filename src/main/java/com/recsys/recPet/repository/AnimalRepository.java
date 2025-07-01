810be194911e4021dfeeb332a37de3b1b01368ec
Milena <elenprojects7@gmail.com>
2025-06-30 21:14:24 -0300

    feat(animal-repository)

---
+++ src/main/java/com/recsys/recPet/repository/AnimalRepository.java
@@ -0,0 +1,11 @@
+package com.recsys.recPet.repository;
+
+import com.recsys.recPet.model.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long>, JpaSpecificationExecutor<Animal> {
+
+}
