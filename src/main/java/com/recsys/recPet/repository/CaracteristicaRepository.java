92d7586cebf4b111399a30875b43973228db0a26
Milena <elenprojects7@gmail.com>
2025-06-30 21:19:10 -0300

    feat(caracteristica-repository):

---
+++ src/main/java/com/recsys/recPet/repository/CaracteristicaRepository.java
@@ -0,0 +1,14 @@
+package com.recsys.recPet.repository;
+
+import com.recsys.recPet.model.Caracteristica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CaracteristicaRepository extends JpaRepository<Caracteristica, Integer> {
+    Optional<Caracteristica> findByNome(String nome);
+    List<Caracteristica> findByNomeIn(List<String> nomes);
+}
