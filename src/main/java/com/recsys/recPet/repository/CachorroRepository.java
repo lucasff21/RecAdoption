e3a948b0b1cb23b39c7d82c0a3501c95a78a7333
Milena <elenprojects7@gmail.com>
2025-07-02 21:30:44 -0300

    remove

--- src/main/java/com/recsys/recPet/repository/CachorroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recsys.recPet.repository;
-
-import com.recsys.recPet.model.Cachorro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CachorroRepository extends JpaRepository<Cachorro, Long> {
-}
