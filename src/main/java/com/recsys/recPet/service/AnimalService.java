57f5bc7a8f110850103f403add5883ba89d89e27
Milena <elenprojects7@gmail.com>
2025-07-13 22:35:01 -0300

    feat: adicionar função para atualizar e adicionar animal, retornar caracteristicas e filtro

--- src/main/java/com/recsys/recPet/service/AnimalService.java
+++ src/main/java/com/recsys/recPet/service/AnimalService.java
@@ -1,6 +1,9 @@
 package com.recsys.recPet.service;
 
+import com.recsys.recPet.dto.animal.AnimalCreateDTO;
+import com.recsys.recPet.dto.animal.AnimalResponseDTO;
 import com.recsys.recPet.dto.animal.AnimalUpdateDTO;
+import com.recsys.recPet.dto.animal.CaracteristicaDTO;
 import com.recsys.recPet.model.Animal;
 import com.recsys.recPet.model.AnimalCaracteristicaId;
 import com.recsys.recPet.model.Caracteristica;
@@ -11,13 +14,12 @@ import com.recsys.recPet.repository.AnimalCaracteristicaRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.data.domain.PageImpl;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -42,11 +44,11 @@ public class AnimalService {
     }
 
     @Transactional(readOnly = true)
-    public Page<Animal> findAll(
+    public PageImpl<AnimalResponseDTO> findAll(
             String nome,
             String sexo,
             String porte,
-            List<String> caracteristicaNomes,
+            List<Long> caracteristicaIds,
             String faixaEtaria,
             Pageable pageable
     ) {
@@ -100,21 +102,28 @@ public class AnimalService {
                 }
             }
 
-            if (caracteristicaNomes != null && !caracteristicaNomes.isEmpty()) {
-                Join<Object, Object> animalCaracteristicasJoin = root.join("animalCaracteristicas");
+            if (caracteristicaIds != null && !caracteristicaIds.isEmpty()) {
+                Join<Animal, AnimalCaracteristica> animalCaracteristicasJoin = root.join("animalCaracteristicas");
                 Join<AnimalCaracteristica, Caracteristica> caracteristicaJoin = animalCaracteristicasJoin.join("caracteristica");
-                predicates.add(caracteristicaJoin.get("nome").in(caracteristicaNomes));
+
+                predicates.add(caracteristicaJoin.get("id").in(caracteristicaIds));
             }
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
 
-        return animalRepository.findAll(spec, pageable);
+        Page<Animal> animalPage = animalRepository.findAll(spec, pageable);
+
+        List<AnimalResponseDTO> dtoList = animalPage.getContent().stream()
+                .map(AnimalResponseDTO::fromEntity)
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(dtoList, pageable, animalPage.getTotalElements());
     }
 
-    public Animal findById(Long id) {
+    public Animal findById(Long id) throws Exception {
         return animalRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Animal não encontrado"));
+                .orElseThrow(() -> new EntityNotFoundException("Animal não encontrado com o ID fornecido."));
     }
 
     @Transactional
@@ -123,66 +132,114 @@ public class AnimalService {
     }
 
     @Transactional
-    public Animal update(@NotNull AnimalUpdateDTO animalDTO, Long id) throws IOException {
-        Animal animal = animalRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Animal não encontrado com o ID fornecido."));
+    public Animal atualizarAnimal(Long idAnimal, AnimalUpdateDTO animalDTO) throws IOException {
+        Animal animalExistente = animalRepository.findById(idAnimal)
+                .orElseThrow(() -> new RuntimeException("Animal não encontrado com o ID: " + idAnimal));
 
-        animalDTO.updateEntity(animal);
+        animalDTO.updateEntity(animalExistente);
 
-        if (animalDTO.getCaracteristicas() != null) {
-            Set<AnimalCaracteristica> novasAssociacoesParaAdicionar = new HashSet<>();
-            List<Caracteristica> caracteristicasExistentes = caracteristicaRepository
-                    .findByNomeIn(animalDTO.getCaracteristicas());
+        List< Long> novosIdsCaracteristicas = animalDTO.getCaracteristicasIds();
 
-            Set<String> nomesCaracteristicasAtuais = animal.getAnimalCaracteristicas().stream()
-                    .map(ac -> ac.getCaracteristica().getNome())
-                    .collect(Collectors.toSet());
-
-            for (Caracteristica carac : caracteristicasExistentes) {
-                if (!nomesCaracteristicasAtuais.contains(carac.getNome())) {
-                    AnimalCaracteristica novaAssociacao = new AnimalCaracteristica();
-                    novaAssociacao.setAnimal(animal);
-                    novaAssociacao.setCaracteristica(carac);
-                    novaAssociacao.setCreatedAt(LocalDateTime.now());
-                    if (animal.getId() != null && carac.getId() != null) {
-                        novaAssociacao.setId(new AnimalCaracteristicaId(animal.getId(), carac.getId()));
-                    }
-                    novasAssociacoesParaAdicionar.add(novaAssociacao);
-                }
-            }
-
-            Set<AnimalCaracteristica> paraRemover = animal.getAnimalCaracteristicas().stream()
-                    .filter(ac -> !animalDTO.getCaracteristicas().contains(ac.getCaracteristica().getNome()))
-                    .collect(Collectors.toSet());
-
-            paraRemover.forEach(animal::removeAnimalCaracteristica);
-            animalCaracteristicaRepository.deleteAll(paraRemover);
-
-            novasAssociacoesParaAdicionar.forEach(animal::addAnimalCaracteristica);
+        if (novosIdsCaracteristicas == null || novosIdsCaracteristicas.isEmpty()) {
+            animalCaracteristicaRepository.deleteAll(animalExistente.getAnimalCaracteristicas());
+            animalExistente.getAnimalCaracteristicas().clear();
+        } else {
+            this.adicionarRemoverCaracteristicas(animalExistente, novosIdsCaracteristicas);
         }
 
-        MultipartFile imagemFile = animalDTO.getNovaImagem();
-        if (imagemFile != null && !imagemFile.isEmpty()) {
-            if (animal.getImagemPath() != null && !animal.getImagemPath().isEmpty()) {
-                try {
-                    String oldFileName = imageService.extrairPublicIdCloudinaryUrl(animal.getImagemPath());
-                    if (oldFileName != null) {
-                        imageService.delete(oldFileName);
-                    }
-                } catch (Exception e) {
-                    throw new IOException("Erro ao excluir a imagem antiga: " + e.getMessage(), e);
-                }
+        if (animalDTO.getNovaImagem() != null) {
+            Map<?, ?> image = this.imageService.uploadImage(animalDTO.getNovaImagem(), "pets/adoption");
+
+            if (!animalExistente.getImagemPath().isEmpty()) {
+                imageService.delete(animalExistente.getImagemPath());
             }
-            Map<?, ?> image = this.imageService.uploadImage(imagemFile, "pets/adoption");
-            animal.setImagemPath((String) image.get("secure_url"));
+
+            imageService.delete(animalExistente.getImagemPath());
+
+            animalExistente.setImagemPath((String) image.get("secure_url"));
         }
 
-        return animalRepository.save(animal);
+        return animalRepository.save(animalExistente);
     }
 
     @Transactional
+    protected void adicionarRemoverCaracteristicas(Animal animal, List<Long> novosIdsCaracteristicas) {
+        Set<AnimalCaracteristica> associacoesAtuais = new HashSet<>(animal.getAnimalCaracteristicas());
+        Set<Long> idsAtuais = associacoesAtuais.stream()
+                .map(ac -> ac.getCaracteristica().getId())
+                .collect(Collectors.toSet());
+
+        Set<Long> novosIdsSet = new HashSet<>(novosIdsCaracteristicas);
+
+        Set<AnimalCaracteristica> paraRemover = associacoesAtuais.stream()
+                .filter(ac -> !novosIdsSet.contains(ac.getCaracteristica().getId()))
+                .collect(Collectors.toSet());
+
+        if (!paraRemover.isEmpty()) {
+            paraRemover.forEach(animal::removeAnimalCaracteristica);
+            animalCaracteristicaRepository.deleteAll(paraRemover);
+        }
+
+        Set<Long> idsParaAdicionar = new HashSet<>(novosIdsSet);
+        idsParaAdicionar.removeAll(idsAtuais);
+
+        if (!idsParaAdicionar.isEmpty()) {
+            List<Caracteristica> caracteristicasParaAdicionar = caracteristicaRepository.findAllById(idsParaAdicionar);
+
+            for (Caracteristica caracteristica : caracteristicasParaAdicionar) {
+
+                AnimalCaracteristicaId acId = new AnimalCaracteristicaId();
+                acId.setAnimalId(animal.getId());
+                acId.setCaracteristicaId(caracteristica.getId());
+
+                AnimalCaracteristica novaAssociacao = new AnimalCaracteristica();
+                novaAssociacao.setId(acId);
+                novaAssociacao.setCaracteristica(caracteristica);
+                animal.addAnimalCaracteristica(novaAssociacao);
+            }
+        }
+    }
+
+    @Transactional
+    public void criarAnimal(AnimalCreateDTO animalDTO) throws IOException {
+        Animal animal = animalDTO.toEntity();
+
+        if (animalDTO.getImagem() != null && !animalDTO.getImagem().isEmpty()) {
+            Map<?, ?> image = this.imageService.uploadImage(animalDTO.getImagem(), "pets/adoption");
+            animal.setImagemPath((String) image.get("secure_url"));
+        }
+
+        Animal savedAnimal = animalRepository.save(animal);
+
+        if (animalDTO.getCaracteristicasIds() != null && !animalDTO.getCaracteristicasIds().isEmpty()) {
+            List<Caracteristica> caracteristicas = caracteristicaRepository.findAllById(animalDTO.getCaracteristicasIds());
+            for (Caracteristica carac : caracteristicas) {
+                AnimalCaracteristica ac = new AnimalCaracteristica();
+                ac.setAnimal(savedAnimal);
+                ac.setCaracteristica(carac);
+                ac.setCreatedAt(LocalDateTime.now());
+
+
+                if (savedAnimal.getId() != null && carac.getId() != null) {
+                    ac.setId(new AnimalCaracteristicaId(savedAnimal.getId(), carac.getId()));
+                }
+                savedAnimal.addAnimalCaracteristica(ac);
+            }
+
+        }
+    }
+
+
+    @Transactional
     public void delete(Long id) {
-        Animal animal = findById(id);
-        animalRepository.delete(animal);
+        Optional<Animal> animal =  animalRepository.findById(id);
+        animal.ifPresent(animalRepository::delete);
+    }
+
+    public List<CaracteristicaDTO> findAllCaracteristicas() {
+        List<Caracteristica> caracteristicas = caracteristicaRepository.findAll();
+        return caracteristicas.stream()
+                .map(CaracteristicaDTO::fromEntity)
+                .collect(Collectors.toList());
     }
 }
