dd6349bb1bde608369af87ae71b6af31ecbac8f1
Milena <elenprojects7@gmail.com>
2025-07-27 22:49:09 -0300

    feat: atualizar usuário

--- src/main/java/com/recsys/recPet/service/UserService.java
+++ src/main/java/com/recsys/recPet/service/UserService.java
@@ -1,16 +1,21 @@
 package com.recsys.recPet.service;
 
 import com.recsys.recPet.dto.*;
-import com.recsys.recPet.dto.usuario.UserResponseDTO;
+import com.recsys.recPet.dto.usuario.UsuarioResponseDTO;
 import com.recsys.recPet.dto.usuario.UsuarioAdotanteCreateDTO;
 import com.recsys.recPet.dto.usuario.UsuarioLoginDTO;
+import com.recsys.recPet.dto.usuario.UsuarioUpdateDTO;
 import com.recsys.recPet.enums.TipoUsuario;
+import com.recsys.recPet.enums.filtro.TipoBusca;
 import com.recsys.recPet.model.Endereco;
 import com.recsys.recPet.model.User;
 import com.recsys.recPet.repository.EnderecoRepository;
 import com.recsys.recPet.repository.UserRepository;
+import com.recsys.recPet.repository.specification.UserSpecification;
 import com.recsys.recPet.security.JwtTokenProvider;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -90,18 +95,10 @@ public class UserService {
         }
     }
 
-    public Page<UserResponseDTO> findAll(String query, TipoUsuario tipoUsuario, Pageable pageable){
-        Page<User> users;
-
-        if (query != null && !query.isEmpty()) {
-            users = userRepository.findByNomeContainingIgnoreCaseOrEmailContainingIgnoreCase(query, query, pageable);
-        } else if (tipoUsuario != null) {
-            users = userRepository.findByTipo(tipoUsuario, pageable);
-        } else {
-            users = userRepository.findAll(pageable);
-        }
-
-        return users.map(UserResponseDTO::new);
+    public Page<UsuarioResponseDTO> findAll(String valor, TipoBusca tipoBusca, TipoUsuario role, Pageable pageable) {
+        Specification<User> spec = UserSpecification.filterBy(valor, tipoBusca, role);
+        Page<User> users = userRepository.findAll(spec, pageable);
+        return users.map(UsuarioResponseDTO::new);
     }
 
     public User findById(Long id){
@@ -117,5 +114,42 @@ public class UserService {
     public User findByEmail(String email){
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+    }
+
+    @Transactional()
+    public UsuarioResponseDTO buscarPerfilPorId(Long usuarioId) {
+        return userRepository.findById(usuarioId)
+                .map(UsuarioResponseDTO::new)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado com o ID: " + usuarioId));
+    }
+    
+    @Transactional
+    public UsuarioResponseDTO atualizarPerfil(Long usuarioId, UsuarioUpdateDTO dto) {
+        User usuarioParaAtualizar = userRepository.findById(usuarioId)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado para atualização"));
+
+        usuarioParaAtualizar.setNome(dto.getNome());
+        usuarioParaAtualizar.setTelefone(dto.getTelefone());
+        usuarioParaAtualizar.setGenero(dto.getGenero());
+        usuarioParaAtualizar.setDataNascimento(dto.getDataNascimento());
+
+        if (dto.getEndereco() != null) {
+            Endereco endereco = usuarioParaAtualizar.getEndereco();
+            if (endereco == null) {
+                endereco = new Endereco();
+                usuarioParaAtualizar.setEndereco(endereco);
+                endereco.setUser(usuarioParaAtualizar);
+            }
+            endereco.setCep(dto.getEndereco().getCep());
+            endereco.setLogradouro(dto.getEndereco().getLogradouro());
+            endereco.setComplemento(dto.getEndereco().getComplemento());
+            endereco.setBairro(dto.getEndereco().getBairro());
+            endereco.setLocalidade(dto.getEndereco().getCidade());
+            endereco.setUf(dto.getEndereco().getEstado());
+        }
+
+        User usuarioSalvo = userRepository.save(usuarioParaAtualizar);
+
+        return new UsuarioResponseDTO(usuarioSalvo);
     }
 }
