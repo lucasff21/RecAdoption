4f448fc99d7090e41d55ca5ca7c69a40566cb608
Milena <elenprojects7@gmail.com>
2025-09-23 23:02:54 -0300

    feat: findByAnimalAndIdNotAndStatusIn e findByIdAndUserId

--- src/main/java/com/recsys/recPet/repository/AdocaoRepository.java
+++ src/main/java/com/recsys/recPet/repository/AdocaoRepository.java
@@ -1,12 +1,17 @@
 package com.recsys.recPet.repository;
 
+import com.recsys.recPet.enums.adocao.AdocaoStatus;
 import com.recsys.recPet.model.Adocao;
+import com.recsys.recPet.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
     List<Adocao> findByUserId(Long userId);
+    List<Adocao> findByAnimalAndIdNotAndStatusIn(Animal animal, Long idExcluido, List<AdocaoStatus> statuses);
+    Optional<Adocao> findByIdAndUserId(Long id, Long userId);
 }
