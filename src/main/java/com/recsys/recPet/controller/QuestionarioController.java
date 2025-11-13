a84d133c66571183e0189e4f88bd61fa1ffe7799
Milena <elenprojects7@gmail.com>
2025-11-12 23:10:33 -0300

    lint

--- src/main/java/com/recsys/recPet/controller/QuestionarioController.java
+++ src/main/java/com/recsys/recPet/controller/QuestionarioController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
-import java.net.URISyntaxException;
 
 
 @RestController
